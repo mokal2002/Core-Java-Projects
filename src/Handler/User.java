2e4af2535e3b22321d1763817d3829e2ca28ed53
Aniket Mokal <79739328+mokal2002@users.noreply.github.com>
2024-08-19 19:26:43 +0530

    Delete src directory

--- src/Handler/User.java
+++
@@ -1,2 +0,0 @@
-package Handler;public class User {
-}
--- src/config/AppContants.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-
-public class AppContants {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/securebank";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "root";
-}
