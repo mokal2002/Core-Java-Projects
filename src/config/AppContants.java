bda86029746fb856ce6b38eaa6b77e94b46667ba
aniketmokal29 <aniketmokal29@gmail.com>
2024-08-19 19:25:19 +0530

    first commit

---
+++ src/config/AppContants.java
@@ -0,0 +1,8 @@
+package config;
+
+
+public class AppContants {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/securebank";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "root";
+}
